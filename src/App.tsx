1e8dc33dcc4528118e927d2ad0597bc3f39dcfdc
zyansaber <zyan112491@gmail.com>
2026-01-05 14:18:15 +1100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-+6
--2
-
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
